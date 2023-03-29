b02ee3f8913ff7793bc03e26f3e628b699d51771
WincerChan <WincerChan@gmail.com>
2023-03-29 22:03:12 +0800

    add smoothing scroll

--- src/components/shared/SvgBackground.jsx
+++ src/components/shared/SvgBackground.jsx
@@ -1,7 +1,50 @@
-import { Show } from "solid-js";
+import { createEffect, createSignal, Show } from "solid-js";
+
+const PartTwoBackground = (props) => {
+    const [scrollDirection, setScrollDirection] = createSignal('down');
+    let lastScrollTop = 0;
+
+
+    createEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    });
+
+    const handleScroll = () => {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        const scrollHeight = document.documentElement.scrollHeight;
+        const clientHeight = document.documentElement.clientHeight;
+
+        if (scrollTop > lastScrollTop) {
+            window.scrollTo({
+                top: scrollHeight - clientHeight,
+                behavior: "smooth"
+            })
+            // 向下滚动
+            setScrollDirection('down');
+        } else {
+            // 向上滚动
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            })
+            setScrollDirection('up');
+        }
+        lastScrollTop = scrollTop
+    };
+
+    return (
+        <div className="bg-cover bg-top h-[146px] 2xl:h-48 absolute bottom-0 z-20 w-full" style={props.style} />
+    );
+
+}
 
 const SvgBackground = (props) => {
     const { position, svgAsset } = props;
+
     let svgBg;
     if (position === "top")
         svgBg = svgAsset.bgTop;
@@ -13,10 +56,10 @@ const SvgBackground = (props) => {
     return (
         <>
             <Show when={position === "bottom"}>
-                <div className="bg-cover bg-top h-[146px] 2xl:h-48 absolute bottom-0 z-20 w-full" style={style} />
+                <PartTwoBackground style={style} />
             </Show>
             <Show when={position === "top"}>
-                <div class="bg-cover bg-center h-36 sm:h-52 2xl:h-64" style={style} />
+                <div class="bg-cover w-full bg-center h-36 sm:h-52 2xl:h-64" style={style} />
             </Show>
         </>
     )
